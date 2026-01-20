bb92bf3a4d2ad757e43a956283aa52c60b1f2ec5
Isi1305 <isabell.sophieandrea@gmail.com>
2026-01-20 13:32:22 +0100

    Implement basic arithmetic functions in Python

---
+++ SimplerTachenrechner.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    result = a + b
+    return result
+def subtract(a, b):
+    result = a - b
+    return result
+def divide(a, b):
+    result = a * b
+    return result
+def divide(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        return "Why."
