5b4fa4599baa9677b89b6dd85ecff640e75b68fc
Isi1305 <isabell.sophieandrea@gmail.com>
2026-01-27 09:30:15 +0100

    Fix typo in read_csv function call

--- Uni.py
+++ Uni.py
@@ -66,7 +66,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_cvs("daten.csv")
+df = pd.read_csv("daten.csv")
 
 print(df.head()) # first 5
 print(df.head(10)) # first 10
